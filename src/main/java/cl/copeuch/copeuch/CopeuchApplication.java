2c20132281edd830461d11c7176f08029148241b
juan.delgadilloquezada <juan.delgadilloquezada@externos-cl.cencosud.com>
2021-05-06 15:19:25 -0400

    Add proyect

---
+++ src/main/java/cl/copeuch/copeuch/CopeuchApplication.java
@@ -0,0 +1,13 @@
+package cl.copeuch.copeuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopeuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopeuchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/copeuch/copeuch/CopeuchApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.copeuch.copeuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopeuchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
